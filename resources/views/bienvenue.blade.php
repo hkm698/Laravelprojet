d0d982688312b53c0673b1dce1ca29b2c1001f63
hkm698 <154525070+hkm698@users.noreply.github.com>
2024-09-05 17:00:24 +0200

    Ajout d'une vue home bienvenue

---
+++ resources/views/bienvenue.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bienvenue</title>
+
+</head>
+
+<body>
+    <H1>BIENVENUE</H1>
+</body>
+
+</html>
